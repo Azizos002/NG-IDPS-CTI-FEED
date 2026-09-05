95cecd72bdd283ca7adde7cd91ab9caa103300b0
Azizos002 <zizoudh06@gmail.com>
2026-09-05 14:06:29 +0100

    SQLite data push
    Seed SQLite database with initial CTI articles

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["better-sqlite3"],
+  allowedDevOrigins: ["192.168.56.1"]
 };
 
 export default nextConfig;
